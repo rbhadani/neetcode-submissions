1b4b2b46ed6f9683ea965fbbe5a54c9361d65b92
rbhadani <62795209+rbhadani@users.noreply.github.com>
2026-05-20 10:16:28 +0530

    Add: find-median-in-a-data-stream - submission-0

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-0.java
@@ -0,0 +1,27 @@
+class MedianFinder {
+    
+    PriorityQueue<Integer> minHeap;
+    PriorityQueue<Integer> maxHeap;
+
+    public MedianFinder() {
+        minHeap = new PriorityQueue<>();
+        maxHeap = new PriorityQueue<>((a,b)->(b-a));
+    }
+    
+    public void addNum(int num) {
+        maxHeap.offer(num);
+
+        minHeap.offer(maxHeap.poll());
+
+        if(minHeap.size()>maxHeap.size()){
+            maxHeap.offer(minHeap.poll());
+        }
+    }
+    
+    public double findMedian() {
+        if(maxHeap.size()>minHeap.size()){
+            return maxHeap.peek();
+        }
+        return (maxHeap.peek()+minHeap.peek())/2.0;
+    }
+}
