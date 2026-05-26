1e1ec06215459ae6c19a734c2dc5917c1315e906
rbhadani <62795209+rbhadani@users.noreply.github.com>
2026-05-26 16:42:00 +0530

    Add: house-robber-ii - submission-14

---
+++ Data Structures & Algorithms/house-robber-ii/submission-14.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int rob(int[] nums) {
+
+        int n = nums.length;
+        if(n==0)return 0;
+        if(n==1)return nums[0];
+        return Math.max(robHouse(0,n-2,nums),robHouse(1,n-1,nums));
+    }
+
+    public int robHouse(int start,int end,int[]nums){
+        int prev2 = 0;
+        int prev1 = 0;
+
+        for(int i=start;i<=end;i++){
+            int take = nums[i]+prev2;
+            int skip = prev1;
+
+            int curr = Math.max(take,skip);
+
+            prev2 = prev1;
+            prev1 = curr;
+
+        }
+        return prev1;
+    }
+}
