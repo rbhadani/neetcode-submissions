39a5fc0c412a4c0f8c29f9ef72c5207746157239
rbhadani <62795209+rbhadani@users.noreply.github.com>
2026-05-26 20:03:08 +0530

    Add: maximum-product-subarray - submission-8

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-8.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+
+        int currMax = 1;
+        int currMin = 1;
+        int res = Integer.MIN_VALUE;
+
+        for(int i=0;i<nums.length;i++){
+
+            if(nums[i]<0){
+                int temp = currMin;
+                currMin = currMax;
+                currMax = temp;
+            }
+
+            currMax = Math.max(nums[i],nums[i]*currMax);
+            currMin = Math.min(nums[i],nums[i]*currMin);
+
+            res = Math.max(res,currMax);
+        }
+        return res;
+    }
+}
