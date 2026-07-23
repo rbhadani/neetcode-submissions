f6610eb9663b64254c321585a58821f59a1f1c88
rbhadani <62795209+rbhadani@users.noreply.github.com>
2026-07-23 13:04:45 +0530

    Add: decode-ways - submission-5

---
+++ Data Structures & Algorithms/decode-ways/submission-5.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numDecodings(String s) {
+        int n = s.length();
+
+        if(n==0 || s.charAt(0)=='0')return 0;
+
+        int[]dp = new int[n+1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i=2;i<=n;i++){
+            int curr = s.charAt(i-1)-'0';
+
+            if(curr>0 && curr<=9){
+                dp[i] +=dp[i-1];
+            }
+
+            int next = Integer.parseInt(s.substring(i-2,i));
+
+            if(next>=10 && next<=26){
+                dp[i]+=dp[i-2];
+            }
+        }
+        return dp[n];
+    }
+}
